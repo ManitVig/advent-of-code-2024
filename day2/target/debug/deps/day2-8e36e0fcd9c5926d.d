51a9c66b30b08708d1a2a69068eed306fc7f1f4c
ManitVig <63181562+ManitVig@users.noreply.github.com>
2024-12-07 10:38:42 +0530

    Delete day2/target directory

--- day2/target/debug/deps/day2-8e36e0fcd9c5926d.d
+++
@@ -1,7 +0,0 @@
-/Users/manitvig/Documents/codeworks/advent-of-code-2024/day2/target/debug/deps/libday2-8e36e0fcd9c5926d.rmeta: src/main.rs src/part1.rs src/part2.rs
-
-/Users/manitvig/Documents/codeworks/advent-of-code-2024/day2/target/debug/deps/day2-8e36e0fcd9c5926d.d: src/main.rs src/part1.rs src/part2.rs
-
-src/main.rs:
-src/part1.rs:
-src/part2.rs:
--- day2/target/debug/deps/day2-e230c184460f7667.d
+++
@@ -1,7 +0,0 @@
-/Users/manitvig/Documents/codeworks/advent-of-code-2024/day2/target/debug/deps/day2-e230c184460f7667: src/main.rs src/part1.rs src/part2.rs
-
-/Users/manitvig/Documents/codeworks/advent-of-code-2024/day2/target/debug/deps/day2-e230c184460f7667.d: src/main.rs src/part1.rs src/part2.rs
-
-src/main.rs:
-src/part1.rs:
-src/part2.rs:
